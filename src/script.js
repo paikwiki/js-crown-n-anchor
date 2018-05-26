9460b7f99442ece1f040a5ae49e97813488306c5
paikwiki <paikwiki@gmail.com>
2018-05-26 10:33:05 +0900

    Fix typo

--- src/script.js
+++ src/script.js
@@ -4,16 +4,16 @@ let funds = 50;
 
 while(funds > 1 && funds < 100) {
   // bet
-  
+
   // rolling dice
-  
+
   // take money if I win
 }
 
 // helper function
 // return random value under n over m
 function rand(m, n) {
-  return m + Math.floor(n - m + 1)*Math.random());
+  return m + Math.floor((n - m + 1)*Math.random());
 }
 
 // return one of six faces
