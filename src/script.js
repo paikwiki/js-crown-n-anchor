d0ecdcda69792d7b179164ae35c4c738f826727a
paikwiki <paikwiki@gmail.com>
2018-05-26 10:30:32 +0900

    Add helper functions

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
 // script.js
+
+let funds = 50;
+
+while(funds > 1 && funds < 100) {
+  // bet
+  
+  // rolling dice
+  
+  // take money if I win
+}
+
+// helper function
+// return random value under n over m
+function rand(m, n) {
+  return m + Math.floor(n - m + 1)*Math.random());
+}
+
+// return one of six faces
+function randFace() {
+  return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0,5)];
+}
